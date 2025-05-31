05fe17064468ea3b953145ed7fdb9aa6e19cdf85
nandana1311 <164927105+nandana1311@users.noreply.github.com>
2025-05-31 16:57:41 +0530

    Update fusioner.dart

--- fusioner.dart
+++ fusioner.dart
@@ -1,1 +1,12 @@
+import 'dart:io';
 
+main() {
+  processList([1, 2, 3, 4], (int num) => num * 2);
+}
+
+void processList(List<int> numbers, int Function(int) operation) {
+  print('\nInput List : $numbers');
+  for (int num in numbers) {
+    print('$num After Operation : ${operation(num)}');
+  }
+}
